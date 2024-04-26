aa80f89cfc0ca76753ddf6ab2b80f82131d5b71d
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-04-26 21:33:38 +0000

    polish lemma script search command enum

--- src/main/java/tla/domain/model/Script.java
+++ src/main/java/tla/domain/model/Script.java
@@ -1,9 +1,5 @@
 package tla.domain.model;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
@@ -18,22 +14,13 @@ public enum Script {
 
     private String id;
 
-    private static Map<String, Script> valueMap = Arrays.stream(
-        Script.values()
-    ).collect(
-        Collectors.toMap(
-            dict -> dict.id,
-            dict -> dict
-        )
-    );
-
     Script(String id) {
         this.id = id;
     }
 
     @JsonCreator
-    public static Script fromString(String id) {
-        return valueMap.get(id);
+    public static Script fromString(String src) {
+        return Script.valueOf(src.toUpperCase());
     }
 
     @Override
@@ -43,14 +30,11 @@ public enum Script {
     }
 
     public static Script ofLemmaId(String id) {
-        char c = id.toLowerCase().charAt(0);
-        if (c == 'c') {
-            return COPTIC;
-        } else if (c == 'd') {
-            return DEMOTIC;
-        } else {
-            return HIERATIC;
-        }
+        return switch (id.toLowerCase().charAt(0)) {
+            case 'c' -> COPTIC;
+            case 'd' -> DEMOTIC;
+            default -> HIERATIC;
+        };
     }
 
 }
